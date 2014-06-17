63bc6c5cf87cfa2ea3162cef79ca72449134376b
Sheheryaar Iqbal <Sheri.3@live.com>
2014-06-17 11:31:10 +0500

    search.php

---
+++ portfolio/search.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying Search Results pages.
+ */
+
+get_header(); ?>
+
+<section id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+
+        <?php if ( have_posts() ) : ?>
+
+            <header class="page-header">
+                <h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'portfolio' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+            </header><!-- .page-header -->
+
+            <?php /* Start the Loop */ ?>
+            <?php while ( have_posts() ) : the_post(); ?>
+
+                <?php get_template_part( 'content', 'home' ); ?>
+
+            <?php endwhile; ?>
+
+            <?php portfolio_paging_nav(); ?>
+
+        <?php else : ?>
+
+            <?php get_template_part( 'content', 'none' ); ?>
+
+        <?php endif; ?>
+
+    </main><!-- #main -->
+</section><!-- #primary -->
+
+<?php get_footer(); ?>
+ 
